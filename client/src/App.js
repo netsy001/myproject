5673751ace7eba3e2cfe188bca73eaee97689aa4
Surendra Choudary Nettam <nettem.surendra@gmail.com>
2021-05-22 14:38:34 +0930

    Added Login page and setting the token by importing the Login component in APp

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
+import React, {useState} from 'react';
 import Nav from './components/Nav';
 import Dashboard from './components/Dashboard';
 import Preferences from './components/Preferences';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
+  const [token, setToken] = useState();
+
+if(!token) {
+  return <Login setToken ={setToken} />
+}
+
   return (
     <div className="App">
       <Nav/>
@@ -13,6 +21,7 @@ function App() {
       <Switch>
         <Route path="/dashboard"><Dashboard/></Route>
         <Route path="/preferences"><Preferences/></Route>
+        {/* <Route path="/login"><Login/></Route> */}
       </Switch>
       </BrowserRouter>
     </div>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import './Login.css';
 export default function Login() {
     return (
-        <div>
-           {/* <h1>Hi</h1>  */}
+        <div className="login">
+            <h3>Please Log In</h3>
+           <form>
+               <label>
+                   <p>Username</p>
+                   <input type="text" />
+               </label>
+               <label>
+                   <p>Password</p>
+                   <input type="password" />
+               </label>
+               <button type="submit">Submit</button>
+           </form>
         </div>
     )
 }

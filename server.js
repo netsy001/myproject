166ed6c1159e144cc70ea8b444eeed1fafb95abe
Surendra Choudary Nettam <nettem.surendra@gmail.com>
2021-05-31 12:39:29 +0930

    type err

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ const PORT = process.env.PORT || 3001;
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
 
-  app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  })
+  // app.get("*", (req,res) => {
+  //   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  // })
 }
 
 mongoose.connect(process.env.MONGO_URI, {

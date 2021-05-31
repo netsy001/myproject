2c1acae2e248096e76516aeea7ff9000d13837f3
Surendra Choudary Nettam <nettem.surendra@gmail.com>
2021-05-31 11:51:01 +0930

    test

--- BackEnd/controllers/user.js
+++ BackEnd/controllers/user.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from 'dotenv';
-import User from '../models/User';
+import User from '../models/User.js';
 dotenv.config();
 
 

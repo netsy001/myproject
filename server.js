41729bdfab140c577f26208a9025c2e49a07e9c7
Surendra Choudary Nettam <nettem.surendra@gmail.com>
2021-05-31 12:52:22 +0930

    type err

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-// import path from 'path';
+const path = require('path');
 import postRoutes from './BackEnd/routes/posts.js';
 import userRoutes from './BackEnd/routes/users.js';
 
@@ -19,11 +19,11 @@ app.use('/user', userRoutes);
 const PORT = process.env.PORT || 3001;
 
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
-  // app.get("*", (req,res) => {
-  //   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  // })
+  app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  })
 }
 
 mongoose.connect(process.env.MONGO_URI, {
